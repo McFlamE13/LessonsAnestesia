23679880deb470c4d6bffb41f59d04eb1b9d7dbe
McFlamE13 <flame138@mail.ru>
2025-06-28 22:31:56 +0300

    fix products

---
+++ goods.py
@@ -0,0 +1,29 @@
+products = [
+    {"id": 1, "price": 100, "category": "A"},
+    {"id": 2, "price": 200, "category": "B"},
+    {"id": 3, "price": 50, "category": "A"},
+    {"id": 4, "price": 1000, "category": "C"},
+    {"id": 5, "price": 250, "category": "A"},
+    {"id": 6, "price": 500, "category": "B"},
+    {"id": 7, "price": 200, "category": "C"},
+    {"id": 8, "price": 4500, "category": "B"}
+]
+m_price = {}
+for product in products:
+    category = product["category"]
+    if category not in m_price:
+        m_price[category] = {"sum": 0, "count": 0}
+    m_price[category]["sum"] += product["price"]
+    m_price[category]["count"] += 1
+for k, v in m_price.items():
+    average = v["sum"] / v["count"]
+    print(f"Average price for {k} - {average}")
+
+max_price = {}
+for product in products:
+    category = product["category"]
+    if category not in max_price or product["price"] > max_price[category]["price"]:
+        max_price[category] = product
+for k, v in max_price.items():
+    print(f"{k} - id = {v['id']} price = {v['price']}")
+
