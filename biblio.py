0bb660fdc1d8d255c36e5d646740894b30092a12
McFlamE13 <flame138@mail.ru>
2025-07-21 18:11:57 +0300

    The end or...

--- biblio.py
+++ biblio.py
@@ -47,12 +47,36 @@ def return_book():
          return
    print("Книга не найдена")
   
-   
-   
-   
-   
-   
- 
+def delete_user():
+   user_id = input("Введите id пользователя для удаления: ")
+   if user_id not in users:
+      print("Пользователь не найден")
+      return
+   if users[user_id]["borrowed"]:
+      print("Нельзя удалить пользователя, у которого есть не возвращенные книги")
+      return
+   del users[user_id]
+   print(f"Пользователь с id {user_id} удален")
+
+def all_books():
+   for k, v in books.items():
+    if v["available"]:
+       print(f"ID: {k}, Название: {v['title']}, Автор книги: {v['author']},  Наличие: 'Есть'")
+    else:
+     print(f"ID: {k},  Название: {v['title']},  Автор книги: {v['author']},  Наличие: 'Нет'")
+
+def available_books():
+   print("Доступные книги:")
+   for k, v in books.items():
+      if v["available"]:
+         print(f"Название: {v['title']}, Автор книги: {v['author']}")
+
+def user_books():
+    print("Книги у пользователей:")
+    for k, v in users.items:
+       if not v["borrowed"]:
+          print(f"Пользователь с ID {k} под именем {v['name']} взял книгу/книги - {v['borrowed']}")
+           
 def add_book():
   title = input("Введите название книги: ")
   author = input("Введите фамилию автора книги: ")
@@ -60,15 +84,46 @@ def add_book():
   books[id] = {"title": title, "author": author, "available": True}
   print(f"Книга c id {id} добавлена")
 
-
-
 books = {
   "B-001": {"title": "Python Basics", "author": "A. Smith", "available": True},
   "B-002": {"title": "Data Science", "author": "B. Johnson", "available": False}
 }
+
 users = {
   "U-100": {"name": "Alice", "borrowed": ["B-002"]},
   "U-101": {"name": "Bob", "borrowed": []}
 }
 
-borrow_book()
+while True:
+    print("\nГлавное меню:")
+    print("1. Добавить книгу")
+    print("2. Добавить пользователя")
+    print("3. Удалить пользователя")
+    print("4. Выдать книгу")
+    print("5. Вернуть книгу")
+    print("6. Показать вообще все списки в библиотеке")
+    print("7. Показать книги в наличиии")
+    print("8. Какой пользователь какие книги взял")
+    print("0. Выход")
+    choice = input("Выберите действие: ")
+    if choice == "1":
+        add_book()
+    elif choice == "2":
+        add_user()
+    elif choice == "3":
+        delete_user()
+    elif choice == "4":
+        borrow_book()
+    elif choice == "5":
+        return_book()
+    elif choice == "6":
+        all_books()
+    elif choice == "7":
+        available_books()
+    elif choice == "8":
+        user_books()
+    elif choice == "0":
+        print("Выход из программы")
+        break
+    else:
+        print("Неверный ввод, попробуйте снова")
