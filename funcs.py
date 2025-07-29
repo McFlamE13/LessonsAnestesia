900633bc2bb59a3c6685f1aa43946a22808539f9
McFlamE13 <flame138@mail.ru>
2025-07-29 21:58:19 +0300

    Add robot

--- funcs.py
+++ funcs.py
@@ -10,3 +10,24 @@ def number(n):
         if n % i == 0:
             return False
     return True
+
+def robot(commands):
+    x, y = 0, 0
+    for direction, steps in commands:
+        if direction == "left":
+            x -= steps
+        elif direction == "right":
+            x += steps
+        elif direction == "forward":
+            y += steps
+        elif direction == "backward":
+            y -= steps
+    return (x, y)
+
+user_commands = [
+    ("right", 5),
+    ("forward", 3),
+    ("left", 2),
+    ("backward", 6)
+]
+print(robot(user_commands))
